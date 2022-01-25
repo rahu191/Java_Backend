963d3dc072dd7c40374755b1ddfe7b91bbce2270
Yash Kasat <32876605+yashkasat21@users.noreply.github.com>
2022-01-25 13:09:34 +0530

    nuking this folder to create repo for new project

--- src/main/java/com/example/AtmApplication/AtmDto/Account_idToAmount.java
+++
@@ -1,9 +0,0 @@
-package com.example.AtmApplication.AtmDto;
-
-import lombok.Data;
-
-@Data
-public class Account_idToAmount {
-    private Long Account_id;
-    private double amount;
-}
--- src/main/java/com/example/AtmApplication/service/mail/MailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.AtmApplication.service.mail;
-
-import com.example.AtmApplication.model.Mail;
-
-public interface MailService {
-    void sendEmail(Mail mail);
-}
